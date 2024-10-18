c9e06eaee96efadb882a4ff3b9d91335863ca953
Tanish Khandelwal <152150452+Tanish1431@users.noreply.github.com>
2024-10-18 23:57:59 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music={
+    "heroine":"https://www.youtube.com/watch?v=oSk3P1mcXTo",
+    "bela":"https://www.youtube.com/watch?v=0aUav1lx3rA",
+    "waka":"https://www.youtube.com/watch?v=pRpeEdMmmQ0",
+    "bapu":"https://www.youtube.com/watch?v=cpKolP6mMec"
+}
